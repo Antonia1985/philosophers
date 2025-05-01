e7c6247549122d4c363bd5c8a0a71fd0eab27d91
Antonia <33874536+Antonia1985@users.noreply.github.com>
2025-05-01 20:21:51 +0200

    Update task.c

--- src/task.c
+++ src/task.c
@@ -1,13 +1,43 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   task.c                                             :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: apavlopo <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/05/01 15:46:18 by apavlopo          #+#    #+#             */
+/*   Updated: 2025/05/01 15:46:25 by apavlopo         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include "philo.h"
+
+void    check_elapsed(t_simulation *sim, pthread_mutex_t *mutex1, pthread_mutex_t *mutex2)
+{
+    struct timeval now;    
+    long elapsed;
+    long timestamp;
+
+    gettimeofday(&now, NULL);
+    get_timestamps(sim, &elapsed, &timestamp);
+    if (elapsed > sim->ph->time_to_die)
+    {        
+        *(sim->die_f) = 1;
+        pthread_mutex_unlock(sim->die_mutex);
+        print_log(sim, " died in check", timestamp);  
+        clean_after_death(mutex1, mutex2);        
+    }
+}
 
 int philo_takes_forks(t_simulation *sim)
 {
-    if (sim->ph->id%2 == 0)
-    {
+    if (sim->ph->id % 2 == 0)
+    {        
         pthread_mutex_lock(sim->ph->left_fork);
         if (!try_log_action_or_die(sim, " has taken the LEFT fork",
                     sim->ph->left_fork, NULL))
             return(0);
+        check_elapsed(sim, sim->ph->left_fork, NULL);
         pthread_mutex_lock(sim->ph->right_fork);
         if (!try_log_action_or_die(sim, " has taken the RIGHT fork",
                 sim->ph->left_fork, sim->ph->right_fork))
@@ -17,13 +47,14 @@ int philo_takes_forks(t_simulation *sim)
     {
         pthread_mutex_lock(sim->ph->right_fork);
         if (!try_log_action_or_die(sim, " has taken the RIGHT fork",
-                sim->ph->left_fork, sim->ph->right_fork))
+                sim->ph->right_fork, NULL))
             return(0);
+        check_elapsed(sim, sim->ph->right_fork, NULL);
         pthread_mutex_lock(sim->ph->left_fork);
         if (!try_log_action_or_die(sim, " has taken the LEFT fork",
-                    sim->ph->left_fork, NULL))
+                    sim->ph->left_fork, sim->ph->right_fork))
             return(0);
-    }    
+    } 
     return(1);
 }
 
@@ -38,11 +69,16 @@ int philo_eats(t_simulation *sim)
     return(1);
 }
 
-int philo_sleeps_and_thinks(t_simulation *sim)
+int philo_sleeps(t_simulation *sim)
 {
     if (!try_log_action_or_die(sim, "is sleeping", sim->log_mutex, NULL))
         return(0);
-    usleep(sim->ph->time_to_sleep * 1000);
+    usleep(sim->ph->time_to_sleep * 1000);    
+    return(1);
+}
+
+int philo_thinks(t_simulation *sim)
+{
     if (!try_log_action_or_die(sim, "is thinking", sim->log_mutex, NULL))
         return(0);
     return(1);
@@ -56,22 +92,37 @@ void	*task(void *args)
     sim = (t_simulation *)args;
     i = 0;
     if (sim->ph->id % 2 == 0)
-		usleep(100);
+    {
+        try_log_action_or_die(sim, "is thinking", sim->log_mutex, NULL);
+        usleep(sim->ph->time_to_eat * 1000);
+    }
+		
     while (((sim->ph->repeat == 0) || i < sim->ph->repeat))
     {
+        pthread_mutex_lock(sim->die_mutex);
+        if (*(sim->die_f))
+        {
+            pthread_mutex_unlock(sim->die_mutex);
+            break;
+        }    
+        pthread_mutex_unlock(sim->die_mutex);
+        
         if(sim->total_ph == 1)
         {
             pthread_mutex_lock(sim->ph->left_fork);
-            try_log_action_or_die(sim, "has taken a fork", sim->ph->left_fork, NULL);
+            try_log_action_or_die(sim, "has taken the LEFT fork", sim->ph->left_fork, NULL);
             usleep(sim->ph->time_to_die * 1000);
             try_log_action_or_die(sim, "died", sim->ph->left_fork, NULL);
             break;
         }
+        check_elapsed(sim, NULL, NULL);
         if(!philo_takes_forks(sim))
             break;
         if (!philo_eats(sim))
             break;        
-        if (!philo_sleeps_and_thinks(sim))
+        if (!philo_sleeps(sim))
+            break;
+        if (!philo_thinks(sim))
             break;
         i++;
     }
@@ -80,4 +131,3 @@ void	*task(void *args)
 	free(sim);
     return (NULL);
 }
-
